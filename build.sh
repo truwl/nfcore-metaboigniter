dca6bb8ba8f0a4a1ed860abc191df253930b1a1e
Jeremy Leipzig <leipzig@gmail.com>
2022-03-14 22:29:37 -0600

    bdcli build

---
+++ build.sh
@@ -0,0 +1,4 @@
+docker build -t truwl/nfcore-metaboigniter:1.0.1_0.1.0 -t truwl/nfcore-metaboigniter:latest .
+docker push truwl/nfcore-metaboigniter:1.0.1_0.1.0
+docker push truwl/nfcore-metaboigniter:latest
+        
